16b438629bfaf2c53b7a2c8e7ea8fd5b4bd30aeb
Ningaanza Remy <remimaradona@gmail.com>
2020-05-10 14:42:00 -0400

    setting up the starter kit

---
+++ assets/js/firstComp/firstComp.js
@@ -0,0 +1,23 @@
+import React, { Component} from 'react'
+import ReactDOM from 'react-dom'
+
+class Layout extends Component {
+  constructor () {
+    super()
+    this.state = {
+      name: 'Joe'
+    }
+  }
+  clickedBtn = () => {
+    console.log('swag')
+  }
+  render () {
+    return (<div className='home'>
+    This is my home page!!
+      </div>)
+  }
+}
+
+const app = document.getElementById('app')
+
+ReactDOM.render(<Layout />, app)

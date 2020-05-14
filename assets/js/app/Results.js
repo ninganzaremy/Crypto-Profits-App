dbce535bfb1d72d6f66d44bb0809c92a6ee08acc
Ningaanza Remy <remimaradona@gmail.com>
2020-05-14 02:13:37 -0400

    added new google adsense

--- assets/js/app/Results.js
+++ assets/js/app/Results.js
@@ -34,7 +34,7 @@ export default class Results extends Component {
            <ins className="adsbygoogle"
      style={{"display":"block"}}
      data-ad-client="ca-pub-1876888588409540"
-     data-ad-slot="6100356041"
+     data-ad-slot="5700576241"
      data-ad-format="auto"></ins>
            </div>
           </div>
